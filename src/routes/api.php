9051de02d5ffeba42bdd50dc700b550d5712e9ae
RenanWth <renanalex2003@hotmail.com>
2025-11-17 20:13:22 -0300

    add rota teste status

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/status', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API está funcionando!',
+        'timestamp' => now()
+    ]);
+});
